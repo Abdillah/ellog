66967126d59fa17e2cda210fe206236246ab5117
Hernawan Fa'iz Abdillah <hernawan.faiz.a@mail.ugm.ac.id>
2016-02-02 13:00:32 +0700

    Add Channel struct and methods prototypes

--- src/Log.hh
+++ src/Log.hh
@@ -2,12 +2,21 @@
 #define __LOG_HH__
 
 namespace Abdillah::Ellog {
+    struct Channel
+    {
+        char* name;
+        bool is_active;
+    };
+
     class Log {
     public:
         static const char* TAG_INFO;
         static const char* TAG_NOTICE;
         static const char* TAG_WARN;
         static const char* TAG_ERROR;
+
+        static Channel create_channel (char* name);
+        static void remove_channel (Channel ch);
 
         Log (const char* channel_name);
         void log (const char* tag, const char* msg);
